ca1eac71c922e407eac1565ab532b353b5b7c687
an.le6 <truongan1730@gmail.com>
2022-11-22 11:05:22 +0700

    2. header animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+        initial={{ opacity: 0, x: -500, scale: 0.5 }}
+        animate={{ opacity: 1, x: 0, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center"
+      >
         <SocialIcon
           url="https://www.linkedin.com/in/truonganln/"
           fgColor="gray"
@@ -22,8 +28,13 @@ const Header = (props: Props) => {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 500, scale: 0.5 }}
+        animate={{ opacity: 1, x: 0, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -31,7 +42,7 @@ const Header = (props: Props) => {
           bgColor="transparent"
         />
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
-      </div>
+      </motion.div>
     </header>
   );
 };
